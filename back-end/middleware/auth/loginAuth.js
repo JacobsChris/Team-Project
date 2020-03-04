51190b8d75a3f46af4972e73909c143a57e8f593
Harr1s96 <hanleyharris96@gmail.com>
2020-03-04 16:36:26 +0000

    separated route and middleware

--- back-end/middleware/auth/loginAuth.js
+++ back-end/middleware/auth/loginAuth.js
@@ -1,14 +1,11 @@
-// const express = require('express');
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-const jwtKey = require('./jwtConfig');
 const userModel = require('../../database/sequelize');
 
-// const router = express.Router();
+const router = express.Router();
 
-passport.use("login", new LocalStrategy(
+const loginAuth = passport.use("login", new LocalStrategy(
 
     function(username, password, done) {
         userModel.findOne({ where: { username: username }})
@@ -29,39 +26,7 @@ passport.use("login", new LocalStrategy(
     }
 ));
 
-// for testing 
-// router.post("/login", function(req, res, next) {
-//     passport.authenticate('login', { session: false },
-//         function(error, user, info) {
-//             if (error) {
-//                 console.log(error);
-//                 res.status(401).send("Error logging in" + " " + error);
-//             }
-//             else if (!user) {
-//                 console.log(error);
-//                 res.status(401).send("Error logging in" + " " + error);
-//             }
-//             else {
-//                 req.logIn(user, (error) => {
-//                         if (error) {
-//                             res.send("Error logging in" + " " + error);
-//                         }
-//                         userModel.findOne({ where: {username: user.username }})
-//                             .then(result => {
-//                                 const token = jwt.sign({ id: result.username }, jwtKey.secret);
-//                                 res.send({
-//                                     auth: true,
-//                                     token: token,
-//                                     message: "user logged in"
-//                                 });
-//                             });
-//                     }
-//                 );
-//             } 
-//     })(req, res, next);
-// });
-
-module.exports = router;
+module.exports = loginAuth;
 
 // bcrypt.compare(password, user.password)
 //                     .then(result => {
