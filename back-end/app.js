33e00387c5a5caaef9a9e9833962492b609dfb32
euan779 <euan779@gmail.com>
2020-03-04 14:48:28 +0000

    all routes ready for middleware addition
    Signed-off-by: euan779 <euan779@gmail.com>

--- back-end/app.js
+++ back-end/app.js
@@ -2,12 +2,15 @@ const express = require("express");
 const app = express();
 const person = require("./routes/person");
 const user = require("./routes/user");
+const vehicle = require("./routes/vehicle");
+const locationEvent = require("./routes/locationEvent");
 const port = 3000;
 
 
-
-app.use("/back-end/person", person);
-
 app.use("/login", user);
+app.use("/back-end/person", person);
+app.use("/back-end/vehicle", vehicle);
+app.use("/back-end/locationEvent", locationEvent);
+
 
 app.listen(port);
--- back-end/routes/person.js
+++ back-end/routes/person.js
@@ -1,27 +1,9 @@
 const express = require("express");
 const router = express.Router();
+
 const test = require("../middleware/service/personService.js");
 
 
-router.use(function (req, res, next) {
-
-    //Auth
-
-    console.log("Auth passed");
-    next()
-}, function (req, res, next) {
-
-    //Validation
-
-    console.log("Validation passed");
-    next()
-}, function (req, res, next) {
-
-    //Service
-
-    console.log("Service passed");
-    next()
-});
 
 router.get("/getData/", function (req, res, next) {
     res.send(test(true));
