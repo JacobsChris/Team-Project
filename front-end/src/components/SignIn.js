b4aad74039e0364028b2186a0aa8449ace0ff852
LiamMcIvor <liam.mcivor1@gmail.com>
2020-03-05 11:48:40 +0000

    Sign in form added

--- front-end/src/components/SignIn.js
+++ front-end/src/components/SignIn.js
@@ -11,7 +11,23 @@ export default class SignIn extends React.Component {
 
     render(){
         return(
-            
+            <div>
+                <form>
+                    <fieldset>
+                        <legend>
+                            Sign In
+                        </legend>
+                        <div className='username'>
+                            <label></label>
+                            <FormInput name='username' value={this.state.username}/>
+                        </div>
+                        <div className='password'>
+                            <label></label>
+                            <FormInput name='password' value={this.state.password}/>
+                        </div>
+                    </fieldset>
+                </form>
+            </div>
         );
     }
 }
