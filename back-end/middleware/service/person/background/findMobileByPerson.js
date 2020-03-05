951b76b25d05a020cb02343658cfb5ff9f75e368
chris <chrisreesjacobs@gmail.com>
2020-03-05 16:12:36 +0000

    changed name of find persons mobile to find mobile by person
    Signed-off-by: chris <chrisreesjacobs@gmail.com>

--- back-end/middleware/service/person/background/findMobileByPerson.js
+++ back-end/middleware/service/person/background/findMobileByPerson.js
@@ -1,7 +1,7 @@
 import {SQLauthenticate} from "./sqlauth";
 
 module.exports = {
-    findPersonMobile: function findPersonsMobile(forenames, surname, DoB, Addr, limit) {
+    findMobileByPerson: function findMobileByPerson(forenames, surname, DoB, Addr, limit) {
         let sqlSearchString = "SELECT phoneNumber FROM mobile WHERE forenames LIKE \'" + forenames + "\' AND surname LIKE \'" + surname + "\' AND address LIKE \'" + Addr + "\' AND dateOfBirth LIKE \'" + DoB + "\' LIMIT " + limit;
         SQLauthenticate(sqlSearchString)
     }
