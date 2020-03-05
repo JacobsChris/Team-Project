cdc676ff629fe72a2c5179051ae96d7ca8182af2
96wilkinson <96wilkinson@sky.com>
2020-03-05 12:58:51 +0000

    config deleted

--- back-end/middleware/service/person/mainSearch.js
+++ back-end/middleware/service/person/mainSearch.js
@@ -5,5 +5,5 @@ const findDetailsByName = require('./background/find_citi_details/findDetailsByN
 
 
 
-// SearchByNames.searchByNames("", "","","","","Male", 10);
-findDetailsByName.findDetailsByName("Stuart", "White","46 FRENSHAM CLOSE, SOUTHALL, UB1 2YG","1948-10-02","STANMORE","Male", 5);
+SearchByNames.searchByNames("", "","","","","Male", 10);
+// findDetailsByName.findDetailsByName("Stuart", "White","46 FRENSHAM CLOSE, SOUTHALL, UB1 2YG","1948-10-02","STANMORE","Male", 5);
