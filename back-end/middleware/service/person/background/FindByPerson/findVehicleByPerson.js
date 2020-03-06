0a670954b1949b7fb3e20907f780cd3b7553b557
96wilkinson <96wilkinson@sky.com>
2020-03-06 18:20:55 +0000

    findVehicleByPerson JSDocs written

--- back-end/middleware/service/person/background/FindByPerson/findVehicleByPerson.js
+++ back-end/middleware/service/person/background/FindByPerson/findVehicleByPerson.js
@@ -1,6 +1,21 @@
 const auth = require('../sqlauth.js');
 
 module.exports = {
+    /**
+     * @author Anthony Wilkinson & Chris
+     *
+     * @function this function takes an input from the function findDetailsByName comprised of which is the forenames,
+     * surname, dateOfBirth, Address and the limit which is hardcoded for the moment
+     *
+     * The function then constructs a string for the MYSQL query to the database through SQLauthenticate (see mysql auth JSDocs
+     * for more info) and then returns it here.
+     *
+     * This is then passed up to findDetailsByName
+     *
+     * @return this returns a JSON object from the MYSQL database
+     *
+     * @requires this function requires string inputs selected from findDetailsByName to function
+     * */
     findVehicleByPerson: function findVehicleByPerson(forenames, surname, DoB, Addr, limit) {
         let sqlSearchString = "SELECT * FROM vehicleRegistration WHERE " +
             "forenames LIKE " + forenames +
