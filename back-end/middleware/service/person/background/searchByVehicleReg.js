301aa701fa70eef662e61644193bef473eb83eac
96wilkinson <96wilkinson@sky.com>
2020-03-06 18:34:49 +0000

    searchByVehicleReg & searchCamerasByArea JSDocs started, unsure exactly how this functon works and don't want to particularly guess so I have set the framework for chris to complete.

--- back-end/middleware/service/person/background/searchByVehicleReg.js
+++ back-end/middleware/service/person/background/searchByVehicleReg.js
@@ -8,7 +8,7 @@ module.exports = {
      * @function this function takes in an input
      *
      *  @return this function returns an JSON object to be passed up
-     *  
+     *
      *  @require this function to work it requires a JSON object to be passed into JsonToStringName()
      * */
     searchByVehicleReg: function searchByVehicleReg(vehicleReg, limit) {
