2a89cd7cbae9437f7d06f38296aec4f4cdf0bcda
euan779 <euan779@gmail.com>
2020-03-06 10:27:44 +0000

    removed unused bodyparsers
    Signed-off-by: euan779 <euan779@gmail.com>

--- back-end/app.js
+++ back-end/app.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const app = express();
-const bodyParser = require("body-parser");
 const passport = require("passport");
 
 const login = require("./routes/login");
@@ -8,14 +7,13 @@ const dataAccess = require("./routes/dataAccess");
 const port = 3000;
 
 
-app.use(express.json());
 app.use(passport.initialize());
 
 passport.serializeUser( function(user, done) {
     done(null, user);
 });
 
-app.use(bodyParser.json());
+
 
 app.use("/back-end", dataAccess);
 app.use("/login", login);
