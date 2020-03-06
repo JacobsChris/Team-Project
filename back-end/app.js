600ffc8ecfeab62bf1308d8f3ddcc67052c6c493
Euan Rossie <45108203+euan779@users.noreply.github.com>
2020-03-06 12:05:27 +0000

    Revert "Back end dev"

--- back-end/app.js
+++ back-end/app.js
@@ -3,11 +3,14 @@ const app = express();
 const bodyParser = require("body-parser");
 const passport = require("passport");
 
+const person = require("./routes/person");
 const login = require("./routes/login");
-const dataAccess = require("./routes/dataAccess");
-const port = 8080;
+const vehicle = require("./routes/vehicle");
+const locationEvent = require("./routes/locationEvent");
+const port = 3000;
 
 
+app.use(express.json());
 app.use(passport.initialize());
 
 passport.serializeUser( function(user, done) {
@@ -15,10 +18,14 @@ passport.serializeUser( function(user, done) {
 });
 
 
+
+
 app.use(bodyParser.json());
 
-app.use("/back-end", dataAccess);
 app.use("/login", login);
+app.use("/back-end/person", person);
+app.use("/back-end/vehicle", vehicle);
+app.use("/back-end/locationEvent", locationEvent);
 
 
 app.listen(port);
