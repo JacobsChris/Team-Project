22b4a09b5c6cb3fa88dc2a841f4c78d927fe3e6c
96wilkinson <96wilkinson@sky.com>
2020-03-06 17:58:30 +0000

    findDetailsByATMId and findATMPoint JSDocs written

--- back-end/middleware/service/person/background/Financial/findATMPointByATM_ID.js
+++ back-end/middleware/service/person/background/Financial/findATMPointByATM_ID.js
@@ -2,6 +2,21 @@ const auth = require('../sqlauth.js');
 
 
 module.exports = {
+    /**
+     * @author Anthony Wilkinson & Chris
+     *
+     * @function this function takes an input from the function findDetailsByATMId comprised of which is the atmId and the limit
+     * which is hardcoded for the moment
+     *
+     * The function then constructs a string for the MYSQL query to the database through SQLauthenticate (see mysql auth JSDocs
+     * for more info) and then returns it here.
+     *
+     * This is then passed up to findDetailsByATMId
+     *
+     * @return this returns a JSON object from the MYSQL database
+     *
+     * @requires this function requires a string input selected from findDetailsByATMId to function
+     * */
     findATMPoint: function findATMPoint(atmId, limit) {
         let sqlSearchString = "SELECT * FROM atmpoint WHERE" +
             " atmId LIKE " + atmId +
