32731e95e56f06859bd422819eba135b1fedc50f
96wilkinson <96wilkinson@sky.com>
2020-03-06 18:12:32 +0000

    findEPOSTransactionsByBankCard & findBankAccountByPerson JSDocs written

--- back-end/middleware/service/person/background/Financial/findEPOSTransactionsByBankCard.js
+++ back-end/middleware/service/person/background/Financial/findEPOSTransactionsByBankCard.js
@@ -1,6 +1,21 @@
 const auth = require('../sqlauth.js');
 
 module.exports = {
+    /**
+     * @author Anthony Wilkinson & Chris
+     *
+     * @function this function takes an input from the function findTransactionsByBankCard comprised of which is the cardNumber
+     * and the limit which is hardcoded for the moment
+     *
+     * The function then constructs a string for the MYSQL query to the database through SQLauthenticate (see mysql auth JSDocs
+     * for more info) and then returns it here.
+     *
+     * This is then passed up to findTransactionsByBankCard
+     *
+     * @return this returns a JSON object from the MYSQL database
+     *
+     * @requires this function requires a string input selected from findTransactionsByBankCard to function
+     * */
     findEPOSTransactions: function findEPOSTransactions(cardNumber, limit) {
         let sqlSearchString = "SELECT * FROM eposTransactions WHERE "+
             " bankCardNumber LIKE " + cardNumber +
