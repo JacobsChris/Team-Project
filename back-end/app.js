c28eb5f31c681507fd95dcdd41c9eb94b3776738
euan779 <euan779@gmail.com>
2020-03-06 10:11:53 +0000

    extracted middleware functionality from person.js to dataAccess.js for reusability
    Signed-off-by: euan779 <euan779@gmail.com>

--- back-end/app.js
+++ back-end/app.js
@@ -3,10 +3,8 @@ const app = express();
 const bodyParser = require("body-parser");
 const passport = require("passport");
 
-const person = require("./routes/person");
 const login = require("./routes/login");
-const vehicle = require("./routes/vehicle");
-const locationEvent = require("./routes/locationEvent");
+const dataAccess = require("./routes/dataAccess");
 const port = 3000;
 
 
@@ -17,15 +15,10 @@ passport.serializeUser( function(user, done) {
     done(null, user);
 });
 
-
-
-
 app.use(bodyParser.json());
 
+app.use("/back-end", dataAccess);
 app.use("/login", login);
-app.use("/back-end/person", person);
-app.use("/back-end/vehicle", vehicle);
-app.use("/back-end/locationEvent", locationEvent);
 
 
 app.listen(port);
--- back-end/routes/person.js
+++ back-end/routes/person.js
@@ -1,19 +1,7 @@
 const express = require("express");
 const router = express.Router();
-const passport = require("passport");
-
-const jwt = require('jsonwebtoken');
-const jwtKey = require('../middleware/auth/jwtConfig');
-const userModel = require('../database/sequelize');
-const endpointAuth = require('../middleware/auth/endpointAuth');
-
-
 
 const mainSearch = require("../middleware/service/person/mainSearch");
-const morgan = require("../middleware/logging/logger");
-
-router.use(passport.authenticate("jwt", {session : false}));
-router.use(morgan);
 
 
 router.get("/getData/", function (req, res, next) {
