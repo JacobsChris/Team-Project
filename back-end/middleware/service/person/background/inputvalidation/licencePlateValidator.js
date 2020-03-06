539ef7cbdece57773840c04eb0ab39c974df9860
96wilkinson <96wilkinson@sky.com>
2020-03-06 18:30:35 +0000

    licencePlateValidator JSDocs started, unsure exactly how this functon works and don't want to particularly guess so I have set the framework for chris to complete.

--- back-end/middleware/service/person/background/inputvalidation/licencePlateValidator.js
+++ back-end/middleware/service/person/background/inputvalidation/licencePlateValidator.js
@@ -1,5 +1,14 @@
 module.exports = {
     licencePlateValidator: function licencePlateValidator(inputString) {
+        /**
+         * @author Chris
+         *
+         * @function this function takes in an input string
+         *
+         * @return this returns the input string with the appropriate syntax
+         *
+         * @requires this function requires an input which is a string
+         * */
         if (inputString.replace(/\s/, "").toUpperCase().match(/^[A-Z_]{2}[0-9_]{2}[A-Z_]{3}$/)) {
             return inputString.replace(/\s/, "").replace(/^(.{4})(.{3})(.*)$/, "$1 $2");
         } else return "Invalid Reg No"
