9db506bef535764219aa2d5ae0ae80c9b49e377b
euan779 <euan779@gmail.com>
2020-03-06 10:24:47 +0000

    changed from body held requests to query level requests
    Signed-off-by: euan779 <euan779@gmail.com>

--- back-end/routes/person.js
+++ back-end/routes/person.js
@@ -5,7 +5,7 @@ const mainSearch = require("../middleware/service/person/mainSearch");
 
 
 router.get("/getData/", function (req, res, next) {
-    mainSearch.JsonToStringName(req.body["requestData"]).then(response => res.send((response)));
+    mainSearch.JsonToStringName(req.query).then(response => res.send((response)));
 });
 
 router.get("/getMatching/");
