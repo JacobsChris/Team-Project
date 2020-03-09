48a8795d37e865872b28f6b9d8977bc2412c9a2e
96wilkinson <96wilkinson@sky.com>
2020-03-09 14:24:40 +0000

    added function where based on a Cell Tower ID you can get a location

--- back-end/middleware/service/person/background/Financial/findDetailsByBankAccount.js
+++ back-end/middleware/service/person/background/Financial/findDetailsByBankAccount.js
@@ -27,7 +27,7 @@ module.exports = {
      *
      *  @require this function to work it requires a JSON object to be passed into JsonToStringBankDetails()
      *  */
-    findBankCardByAccountId: function findBankCardByAccountId(bankAccountId, accountNumber, bank, forenames, forenames, dateOfBirth, homeAddress) {
+    findBankCardByAccountId: function findBankCardByAccountId(bankAccountId, accountNumber, bank, forenames, surname, dateOfBirth, homeAddress) {
         bankAccountId = wildStr.addWildStr(bankAccountId);
         return Promise.all([bankCard.findBankCard(bankAccountId)]);
 
