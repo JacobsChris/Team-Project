1ef32bb7cc3d5b71c462e21c7d49cf5505996d40
LiamMcIvor <liam.mcivor1@gmail.com>
2020-03-09 15:26:41 +0000

    import for create user

---
+++ front-end/src/components/createUser.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+import FormInput from './FormInput';
+
+class CreateUser extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            username: '',
+            verUsername: '',
+            password: '',
+            verPassword: ''
+        }
+    }
+
+    handleChange = ({target: {name, value}}) => {
+        this.setState({
+            [name]: value
+        });
+    }
+
+    submit = () => {
+
+    }
+
+    render() {
+        return (
+            <div>
+                <form onSubmit={this.submit}>
+                    <fieldset>
+                        <legend>
+                            Create User
+                        </legend>
+                        <div className='username'>
+                            <label></label>
+                            <FormInput name='username' value={this.state.username} handleChange={this.handleChange}/>
+                        </div>
+                        <div className='username'>
+                            <label></label>
+                            <FormInput name='verUsername' value={this.state.verUsername} handleChange={this.handleChange}/>
+                        </div>
+                        <div className='password'>
+                            <label></label>
+                            <FormInput name='password' type="password" value={this.state.password} handleChange={this.handleChange}/>
+                        </div>
+                        <div className='password'>
+                            <label></label>
+                            <FormInput name='verPassword' type="password" value={this.state.verPassword} handleChange={this.handleChange}/>
+                        </div>
+                        <input type="submit" value="Create User" />
+                    </fieldset>
+                </form>
+            </div>
+        )
+    }
+}
+
+export default CreateUser;
