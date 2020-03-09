0d4a776b2bbd39a51a300d0b0f394bf17b2105cf
LiamMcIvor <liam.mcivor1@gmail.com>
2020-03-09 14:45:53 +0000

    Route guards added

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -8,20 +8,31 @@ import SearchNavBar from './components/searchNavBar';
 import PeopleResultsPage from './components/peopleResultsPage';
 import SignIn from './components/SignIn';
 import { connect } from 'react-redux';
+import { GuardProvider, GuardedRoute } from 'react-router-guards';
 
 
 function App() {
 
+  const requireLogin = (to, from, next) => {
+    if (sessionStorage.getItem('jwt')) {
+      next();
+    }
+    next.redirect('/user/home/signin');
+  };
+
   return (
     <BrowserRouter>
       <Route path='/user/' component={NavBar}></Route>
-      <Route path='/user/home/' component={SearchNavBar}></Route>
-      <Route path='/user/home/searchpeople' component={SearchPeople}></Route>
-      <Route path='/admin/' component={AdminNavBar}></Route>
-      <Route path='/user/home/peopleresults' component={PeopleResultsPage}></Route>
+        <GuardProvider guards={[requireLogin]}>
+          <GuardedRoute path='/user/home/' component={SearchNavBar}></GuardedRoute>
+          <GuardedRoute path='/user/home/searchpeople' component={SearchPeople}></GuardedRoute>
+          <GuardedRoute path='/admin/' component={AdminNavBar}></GuardedRoute>
+          <GuardedRoute path='/user/home/peopleresults' component={PeopleResultsPage}></GuardedRoute>
+        </GuardProvider>
       <Route path='/user/home/signin' component={SignIn}></Route>
     </BrowserRouter>
   );
 }
+
 
 export default connect ()(App);
