31beca17cf981e11e723793230c6c925d1e26911
LiamMcIvor <liam.mcivor1@gmail.com>
2020-03-09 12:47:35 +0000

    auth added for sign in

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -7,30 +7,21 @@ import AdminNavBar from './components/adminNavBar';
 import SearchNavBar from './components/searchNavBar';
 import PeopleResultsPage from './components/peopleResultsPage';
 import SignIn from './components/SignIn';
-import store from './js/store';
+import { connect } from 'react-redux';
+
 
 function App() {
-  const [auth, setAuth] = useState(false);
 
-  store.subscribe
-  console.log(store.getState().token.length)
-  if(store.getState().token.length){
-    setAuth(true);
-  }
   return (
     <BrowserRouter>
-      {/* <Switch> */}
       <Route path='/user/' component={NavBar}></Route>
-      {!store.getState().token ?
-      <Route path='/user/home/' component={SearchNavBar}></Route> : null}
+      <Route path='/user/home/' component={SearchNavBar}></Route>
       <Route path='/user/home/searchpeople' component={SearchPeople}></Route>
       <Route path='/admin/' component={AdminNavBar}></Route>
       <Route path='/user/home/peopleresults' component={PeopleResultsPage}></Route>
       <Route path='/user/home/signin' component={SignIn}></Route>
-      
-      {/* </Switch> */}
     </BrowserRouter>
   );
 }
 
-export default App;
+export default connect ()(App);
