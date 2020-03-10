69d1b74302957572eb2ebfc9aaa01ab94818600c
LiamMcIvor <liam.mcivor1@gmail.com>
2020-03-10 11:56:38 +0000

    signin refactor

--- front-end/src/js/actions/index.js
+++
@@ -1,6 +0,0 @@
-import { ADD_TOKEN } from "../constants/actions-types";
-
-
-export function addToken(payload) {
-    return { type: "ADD_TOKEN", payload }
-  };
--- front-end/src/js/store/index.js
+++
@@ -1,9 +0,0 @@
-import { createStore, compose } from "redux";
-import rootReducer from "../reducers/index";
-
-const store = createStore(
-    rootReducer
-);
-
-export default store;
-
