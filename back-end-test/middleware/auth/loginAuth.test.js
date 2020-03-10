85d89093b68a54ab0660d9e0f46fc301466fde47
Harr1s96 <hanleyharris96@gmail.com>
2020-03-10 16:06:13 +0000

    fully working login auth tests

--- back-end-test/middleware/auth/loginAuth.test.js
+++ back-end-test/middleware/auth/loginAuth.test.js
@@ -8,32 +8,35 @@ const goodUser = {
 
 const badUser = {
     "username": "root",
-    "username": "root"
+    "password": "root"
 };
 
 const success = "user logged in";
 
-const failure = "unauthorized";
+const failure = "Unauthorized";
+
+const server = app;
 
 describe('login test', function() {
 
     afterEach(function(done) {
-        const server = app;
         server.close();
         done();
-    })
+    });
 
-    it("takes in correct username and password and should return success message", async () => {
+    it("takes in correct username and password and should return success message", async (done) => {
         let res = await request(app)
             .post("/login")
             .send(goodUser);
         expect(res.body.message).toBe(success);
+        done();
     });
 
-    it("takes in invalid username and password and should return failure message", async () => {
+    it("takes in invalid username and password and should return failure message", async (done) => {
         let res = await request(app)
             .post("/login")
             .send(badUser);
         expect(res.text).toBe(failure);
+        done();
     });
 });
