8ea5a30dd3aea0420cea235f8608a9da7aceae10
euan779 <euan779@gmail.com>
2020-03-10 14:14:32 +0000

    made personFullDetails.js work with multiple phones, vehicles and banks registered to a single person. removed unneeded vehicle endpoint api

--- back-end/middleware/service/person/background/personFullDetails.js
+++ back-end/middleware/service/person/background/personFullDetails.js
@@ -1,13 +1,29 @@
 const mainSearch = require("../mainSearch");
 
 module.exports = async function (input) {
-    const [[citizen], [bankAccount], [mobiles], [vehicle]] = await mainSearch.JsonToStringDetails(input);
-    const bankDetails = bankAccount && bankAccount.length > 1 ? await mainSearch.JsonToStringBankDetails(bankAccount, 10) : [];
-    const transactions = bankDetails && bankDetails.length > 1 ? await mainSearch.JsonToStringTransactions(bankDetails[0][0]) : [];
-    const calls = mobiles && mobiles.length > 1 ? await mainSearch.JsonToStringMobileCallRecords(mobiles) : [];
-    const callsLocation = calls && calls.length > 1 ? await mainSearch.JsonToStringCellTowerLocation(calls[0], 1) : [];
-    const callHistory = mobiles && mobiles.length > 1 ? await mainSearch.JsonToCallHistory(mobiles) : [];
-    console.log(mobiles, callHistory);
+
+    let citizen = [], mobiles = [], bankAccount = [], vehicle = [], bankDetails = [], transactions = [],
+        callsLocation = [], callHistory = [], acquaintances = [];
+
+    [[citizen], [bankAccount], [mobiles], [vehicle]] = await mainSearch.JsonToStringDetails(input);
+    for (let i = 0; i < bankAccount.length; i++) {
+        bankDetails += bankAccount && bankAccount.length > 1 ? await mainSearch.JsonToStringBankDetails(bankAccount, 10) : [];
+    }
+    for (let i = 0; i < bankDetails.length; i++) {
+        transactions += bankDetails && bankDetails.length > 1 ? await mainSearch.JsonToStringTransactions(bankDetails[0][0]) : [];
+    }
+    // for (let i = 0; i < mobiles.length; i++) {
+    //     calls += mobiles && mobiles.length > 1 ? await mainSearch.JsonToStringMobileCallRecords(mobiles) : [];
+    // }
+    // for (let i = 0; i < mobiles.length; i++) {
+    //     callsLocation += calls && calls.length > 1 ? await mainSearch.JsonToStringCellTowerLocation(calls[0], 1) : [];
+    // }
+    for (let i = 0; i < mobiles.length; i++) {
+        callHistory += mobiles && mobiles.length > 1 ? await mainSearch.JsonToCallHistory(mobiles) : [];
+    }
+    for (let i = 0; i < callHistory.length; i++) {
+        acquaintances += callHistory && callHistory.length > 1 ? await mainSearch.JsonToPersonByMobile(callHistory) : [];
+    }
 
 
     return {
@@ -17,9 +33,10 @@ module.exports = async function (input) {
         "vehicleData": vehicle,
         "bankDetailsData": bankDetails,
         "transactionsData": transactions,
-        "phoneCallsData": calls,
-        "callsLocationData": callsLocation,
-        "callHistoryData": callHistory
+        // "phoneCallsData": calls,
+        // "callsLocationData": callsLocation,
+        "callHistoryData": callHistory,
+        "acquaintancesData": acquaintances
     }
 
 
