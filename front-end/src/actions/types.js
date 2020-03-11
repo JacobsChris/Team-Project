42bedc272014934b5b4ca370ff19c4bb0de7c2c3
Liam <liam.mcivor1@gmail.com>
2020-03-11 10:39:49 +0000

    Redux refactor

--- front-end/src/actions/types.js
+++
@@ -1,2 +0,0 @@
-export const GET_PEOPLE = 'GET_PEOPLE';
-export const GET_VEHICLE = 'GET_VEHICLE';
--- front-end/src/redux/actions/types.js
+++ front-end/src/redux/actions/types.js
@@ -1,3 +1,4 @@
 export const GET_PEOPLE = 'GET_PEOPLE';
 export const SET_RESULTS_LOADING = 'SET_RESULTS_LOADING';
+export const GET_VEHICLE = 'GET_VEHICLE';
 
