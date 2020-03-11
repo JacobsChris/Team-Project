c294576dbe40dcebc37654f1bf73e1c66961be50
euan779 <euan779@gmail.com>
2020-03-11 14:23:19 +0000

    fixed undefined promise.length error

--- back-end/middleware/service/person/background/personFullDetails.js
+++ back-end/middleware/service/person/background/personFullDetails.js
@@ -7,21 +7,26 @@ module.exports = async function (input) {
 
     [[citizen], [bankAccount], [mobiles], [vehicle]] = await mainSearch.JsonToStringDetails(input);
 
-    for (let i = 0; i < bankAccount.length; i++) {
-        bankDetails += bankAccount && bankAccount.length > 1 ? await mainSearch.JsonToStringBankDetails(bankAccount, 10) : [];
+    if (bankAccount != undefined) {
+        for (let i = 0; i < bankAccount.length; i++) {
+            bankDetails += bankAccount && bankAccount.length > 1 ? await mainSearch.JsonToStringBankDetails(bankAccount, 10) : [];
+        }
     }
-
-    for (let i = 0; i < bankDetails.length; i++) {
-        transactions += bankDetails && bankDetails.length > 1 ? await mainSearch.JsonToStringTransactions(bankDetails[0][0]) : [];
+    if (bankDetails != undefined) {
+        for (let i = 0; i < bankDetails.length; i++) {
+            transactions += bankDetails && bankDetails.length > 1 ? await mainSearch.JsonToStringTransactions(bankDetails[0][0]) : [];
+        }
     }
-
-    for (let i = 0; i < mobiles.length; i++) {
-        callHistory += mobiles && mobiles.length > 1 ? await mainSearch.JsonToCallHistory(mobiles) : [];
+    if (mobiles != undefined) {
+        for (let i = 0; i < mobiles.length; i++) {
+            callHistory += mobiles && mobiles.length > 1 ? await mainSearch.JsonToCallHistory(mobiles) : [];
+        }
     }
-    for (let i = 0; i < callHistory.length; i++) {
-        acquaintances += callHistory && callHistory.length > 1 ? await mainSearch.JsonToPersonByMobile(callHistory) : [];
+    if (callHistory != undefined) {
+        for (let i = 0; i < callHistory.length; i++) {
+            acquaintances += callHistory && callHistory.length > 1 ? await mainSearch.JsonToPersonByMobile(callHistory) : [];
+        }
     }
-
     return {
         "citizenData": citizen,
         "bankAccountData": bankAccount,
