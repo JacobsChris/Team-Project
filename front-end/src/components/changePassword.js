6f3c26e2265ab9c3b304ff563bd3272e2831d5e9
Liam <liam.mcivor1@gmail.com>
2020-03-12 09:52:20 +0000

    change password init

---
+++ front-end/src/components/changePassword.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react'
+
+class ChangePassword extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+
+        }
+    }
+
+    handleChange = () => {
+
+    }
+
+    submit = () => {
+
+    }
+    
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default ChangePassword;
