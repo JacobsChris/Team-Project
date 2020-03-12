d0dc9561ab821fc75963b2565eb8a23a76dd8b5f
chris <chrisreesjacobs@gmail.com>
2020-03-12 15:08:59 +0000

    wrote findBankAccountByPerson.test.js

---
+++ back-end-test/middleware/service/person/background/findByPerson/findBankAccountByPerson.test.js
@@ -0,0 +1,31 @@
+const findBankAccountByPerson = require("../../../../../../back-end/middleware/service/person/background/FindByPerson/findBankAccountByPerson");
+
+
+let inputVal = {
+    "citizenID": "",
+    "forenames": "Gillian Kathryn",
+    "surname": "Newton",
+    "homeAddress": "",
+    "dateOfBirth": "",
+    "placeOfBirth": "",
+    "sex": ""
+};
+
+let outputVal = {
+    "forenames": "Gillian Kathryn",
+    "homeAddress": "30 CASTLE STREET, GUILDFORD, GU1 3UW",
+    "surname": "Newton",
+    "accountNumber": 75482888,
+    "bank": "Citibank International",
+    "bankAccountId": 89368,
+    "dateOfBirth": "1953-11-07",
+
+};
+
+test('find a person\'s full details from partial details', (done) => {
+    findBankAccountByPerson(inputVal)
+        .then(([person]) => {
+            expect(person).toStrictEqual(outputVal);
+            done();
+        })
+});
