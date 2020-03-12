bffa8389a0a8c6b343439e6170fa749a20de2134
Liam <liam.mcivor1@gmail.com>
2020-03-12 09:49:51 +0000

    user page init

---
+++ front-end/src/components/users.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react'
+
+class Users extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+
+        }
+    }
+
+    handleChange = () => {
+
+    }
+
+    submit = () => {
+
+    }
+    
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default Users;
