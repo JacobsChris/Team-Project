58fa64f5897e2333db8ec7589ea749ca000a6a28
chris <chrisreesjacobs@gmail.com>
2020-03-12 15:28:06 +0000

    findATMPointByATM_ID.test.js written

--- back-end/middleware/service/person/background/Financial/findDetailsByATMId.js
+++ back-end/middleware/service/person/background/Financial/findDetailsByATMId.js
@@ -28,7 +28,7 @@ module.exports =
      *  @require this function to work it requires a JSON object to be passed into JsonToStringATM()
      *  */
     function findATMPointByATMId(timestamp, atmId, bankCardNumber, type, amount) {
-        atmId = exactStr.addExactStr(atmId);
-        return Promise.all([atmID.findATMPoint(atmId)]);
+        atmId = exactStr(atmId);
+        return Promise.all([atmID(atmId)]);
 
 };
