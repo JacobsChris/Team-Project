ae65f9ba380889f1716dedc97575cbf407e6262f
Harr1s96 <hanleyharris96@gmail.com>
2020-03-13 13:28:44 +0000

    working logger with dynamoDB

--- back-end/middleware/logging/authLogger.js
+++ back-end/middleware/logging/authLogger.js
@@ -1,5 +1,20 @@
 const morgan = require('morgan');
-const winston = require('./winston');
+const winston = require('winston');
+const { DynamoDB } = require('winston-dynamodb');
+const options = require('./AuthDB.json')
+
+const logger = winston.createLogger({
+    level: "info",
+    format: winston.format.json(),
+    transports: [new winston.transports.DynamoDB(options)],
+    exitOnError: false
+});
+
+logger.stream = {
+    write: function(message) {
+        logger.info(message);
+    }  
+};
 
 morgan.token("login-request", function(req, res) {
     let logObj = {
@@ -9,4 +24,4 @@ morgan.token("login-request", function(req, res) {
     return JSON.stringify(logObj)
 });
 
-module.exports = morgan(":remote-addr :login-request :date[web] :status", { stream: winston.stream });
+module.exports = morgan(":remote-addr :login-request :date[web] :status", { stream: logger.stream });
