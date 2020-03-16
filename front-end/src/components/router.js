91e2cfb5b21072c8dd133b885ab14ff50783e6a4
Liam <liam.mcivor1@gmail.com>
2020-03-16 15:33:16 +0000

    location results added to router

--- front-end/src/components/router.js
+++ front-end/src/components/router.js
@@ -16,6 +16,7 @@ import SearchVehicle from './searchVehicle';
 import VehicleResultsPage from './vehicleResultsPage';
 import ChangePassword from './changePassword';
 import PersonLocation from './personLocation';
+import LocationResults from './LocationResults';
 
 
 class Router extends React.Component {
@@ -57,6 +58,7 @@ class Router extends React.Component {
                     <GuardedRoute path='/user/home/peopleresults' component={PeopleResultsPage} />
                     <GuardedRoute path='/user/home/vehicleresults' component={VehicleResultsPage} />
                     <GuardedRoute path='/user/home/personlocation' component={PersonLocation} />
+                    <GuardedRoute path='/user/home/locationresults' component={LocationResults} />
                 </GuardProvider>
                 <GuardProvider guards={[this.isAdmin]}>
                     <GuardedRoute path='/admin/' component={AdminNavBar} />
@@ -69,6 +71,7 @@ class Router extends React.Component {
                     <GuardedRoute path='/admin/users' component={Users} />
                     <GuardedRoute path='/admin/changepassword' component={ChangePassword} />
                     <GuardedRoute path='/admin/personlocation' component={PersonLocation} />
+                    <GuardedRoute path='/admin/locationresults' component={LocationResults} />
                 </GuardProvider>
                 <Route path='/user/signin' component={SignIn}></Route>
             </BrowserRouter>
