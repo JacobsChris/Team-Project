8570b7017b8a8cd6994359ea7c8a9781f5cbaaac
Harr1s96 <hanleyharris96@gmail.com>
2020-03-16 15:47:42 +0000

    completed refactoring of project structure

--- functions/back-end/routes/dataAccess.js
+++ functions/back-end/routes/dataAccess.js
@@ -7,6 +7,7 @@ const endpointAuth = require('../middleware/auth/endpointAuth');
 const person = require("./person");
 const vehicle = require("./vehicle");
 const locationEvent = require("./locationEvent");
+const geolocation = require("./geolocation");
 
 router.use(morgan);
 passport.use("jwt", endpointAuth);
@@ -15,5 +16,6 @@ router.use(passport.authenticate("jwt", {session : false}));
 router.use("/person", person);
 router.use("/vehicle", vehicle);
 router.use("/locationEvent", locationEvent);
+router.use("/geolocation", geolocation);
 
 module.exports = router;
