b52cdd790ecebc09b6d2ce27c21f173d1a6e4a18
euan779 <euan779@gmail.com>
2020-03-17 10:33:50 +0000

    getMatching updated so it works for GCP. needs development on ordering
    Signed-off-by: euan779 <euan779@gmail.com>

--- functions/back-end/middleware/service/PhoneData/findIncomingCallsByTargetPhoneNumber.js
+++ functions/back-end/middleware/service/PhoneData/findIncomingCallsByTargetPhoneNumber.js
@@ -7,10 +7,10 @@ module.exports =
      * @param phoneNumber
      * @returns the phone numbers that hav called the given input phone number
      */
-     function findInComingCalls(input) {
-        let sqlSearchString = "select *, count(callerNumber) from mobileCallRecords where" +
-            " receiverNumber LIKE " + exactStr(input.phoneNumber) +
-            " GROUP BY callerNumber" +
-            " ORDER BY COUNT(callerNumber) DESC";
+    function findInComingCalls(input) {
+        let sqlSearchString = "select * from mobileCallRecords where" +
+            " receiverNumber LIKE " + exactStr(input.phoneNumber);
+        // " GROUP BY callerNumber" +
+        // " ORDER BY COUNT(callerNumber) DESC";
         return auth(sqlSearchString)
-};
+    };
--- functions/back-end/middleware/service/PhoneData/findOutGoingCallsByTargetPhoneNumber.js
+++ functions/back-end/middleware/service/PhoneData/findOutGoingCallsByTargetPhoneNumber.js
@@ -8,10 +8,10 @@ module.exports =
      * @returns returns the numbers called by the given inputted phone number
      */
      function findOutGoingCalls(input) {
-        let sqlSearchString = "select *, count(receiverNumber) from mobileCallRecords where" +
-            " callerNumber LIKE " + exactStr(input.phoneNumber) +
-            " GROUP BY receiverNumber" +
-            " ORDER BY COUNT(receiverNumber) DESC";
+        let sqlSearchString = "select *from mobileCallRecords where" +
+            " callerNumber LIKE " + exactStr(input.phoneNumber);
+            // " GROUP BY receiverNumber" +
+            // " ORDER BY COUNT(receiverNumber) DESC";
         return auth(sqlSearchString)
 
 
